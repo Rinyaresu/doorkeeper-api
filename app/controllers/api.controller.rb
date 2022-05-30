26c636e8ea1e1d65184fad654b32a3d6eb2860b1
Rinyaresu <kaiquelinhares17@outlook.com>
2022-05-30 16:15:54 -0300

    feat: api controller

---
+++ app/controllers/api.controller.rb
@@ -0,0 +1,11 @@
+class ApiController < ApplicationController
+  before_action :doorkeeper_authorize!
+  skip_before_action :verify_authenticity_token
+  reponse :json
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: doorkeeper_token[:resource_owner_id])
+  end
+end
